a9cdb437a24a5b17c3269dadf5d1520422a4f049
Ani Narzullo <76449446+Anvar-MITC2020@users.noreply.github.com>
2022-06-19 08:59:16 +0500

    CHANGE Message

--- action.php
+++ action.php
@@ -1,10 +1,16 @@
 <?php
-$to      = 'freefreelancer@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$to = 'freefreelancer@gmail.com';
+
+$subject = 'KINGDOM STUDY Message';
+
+$headers  = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "Reply-To: " . strip_tags($_POST['req-email']) . "\r\n";
+$headers .= "CC: susan@example.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+$message = '<p><strong>This is strong text</strong> while this is not.</p>';
+
 
 mail($to, $subject, $message, $headers);
 ?>
